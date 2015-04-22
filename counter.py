85e61036b675cbcd4e135ff4c28bc83f7c6e5c0c
nicolezhu <nzhu94@gmail.com>
2015-04-22 16:57:03 -0500

    get scraped tweets with word frequencies into new json

--- counter.py
+++ counter.py
@@ -5,11 +5,30 @@ from pprint import pprint
 with open('articles/brotherorange.json') as data_file:
 	data = json.load(data_file)
 
-tweet = data[0]['tweet'].split()
-c = collections.Counter(tweet)
+tweet_frequencies = []
 
-pprint(tweet)
-# print len(data)
+for result in data:
+	tweet_text = result['tweet']
+	cleaned_tweet = tweet_text.split()
 
-for word in tweet:
-	print '%s : %d' % (word, c[word])
+	c = collections.Counter(cleaned_tweet)
+
+	pprint(tweet_text)
+
+	unique_words = list(set(cleaned_tweet))
+
+	tweet = {}
+	tweet['tweet'] = tweet_text
+	freq_dict = {}
+
+	for word in unique_words:
+		freq_dict[word] = c[word]
+		print '%s : %d' % (word, c[word])
+
+	tweet['tweet_dict'] = freq_dict
+	print tweet
+
+	tweet_frequencies.append(tweet)
+
+subjects_file = open("articles/brotherorangedict.json", "w")
+print >> json.dump(tweet_frequencies, subjects_file, indent=4)
